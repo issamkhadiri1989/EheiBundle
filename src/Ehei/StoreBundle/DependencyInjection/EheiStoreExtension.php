086c5fb17a1aa2ef3a61918ba27930c9f49e1cf7
Issam KHADIRI <khadiri.issam@gmail.com>
2021-05-14 12:29:23 +0000

    commit

--- src/Ehei/StoreBundle/DependencyInjection/EheiStoreExtension.php
+++ src/Ehei/StoreBundle/DependencyInjection/EheiStoreExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Ehei\StoreBundle\DependencyInjection;
+namespace Ehei\StoreBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
