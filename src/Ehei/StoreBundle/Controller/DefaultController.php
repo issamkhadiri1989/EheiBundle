834ee07c121d19ee9f7e6abd0028cc94224136cb
Issam KHADIRI <khadiri.issam@gmail.com>
2021-05-14 12:41:00 +0000

    commit

--- src/Ehei/StoreBundle/Controller/DefaultController.php
+++ src/Ehei/StoreBundle/Controller/DefaultController.php
@@ -4,19 +4,15 @@ declare(strict_types=1);
 
 namespace Ehei\StoreBundle\Controller;
 
-use App\Ehei\StoreBundle\Service\Store;
+use Ehei\StoreBundle\Service\Store;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * @Route("/ehei", name="ehei_store_")
- */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
      * @param Store $store
+     *
      * @return Response
      */
     public function index(Store $store)
